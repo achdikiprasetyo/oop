aa412128915f7ffeaed59b6bf76a50e9caca4336
Diki's Login <dikiprasetyo1234@gmail.com>
2023-03-13 12:38:49 +0700

    multiple constructor

---
+++ 5. tugas 4 multiple constructor/1. Kode Program tiap project/5. kubus/Tugas4.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tugas4;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Tugas4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        kubus kotak = new kubus(10);
+        kubus dufault_kotak = new ();
+
+        System.out.println("Kubus dengan sisi"+kotak.sisi+" adalah"+kotak.getVolume());
+        System.out.println("Kubus dengan sisi"+kotak.sisi+" adalah"+_default_kotak.getVolume());
+    }
+    
+}
